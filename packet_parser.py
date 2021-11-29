7cc15a107d65d3acb00eda0be6b770bf64f8e839
ak5193 <81817482+ak5193@users.noreply.github.com>
2021-11-28 22:45:23 -0500

    Update packet_parser.py

--- packet_parser.py
+++ packet_parser.py
@@ -1,4 +1,21 @@
-def parse() :
-	print('called parse function in packet_parser.py')
+from filter_packets import *
+
+# taking file that needs to be parsed and outputting parsed list
+def parse(fileName, list):
+    filtered = open(fileName, "r")
+    line = filtered.readline()
+
+    while line:
+        list.append(line.strip().split())
+        line = filtered.readline()
+
+    filtered.close()
+
+# test
+filter()
+file = "Node1_filtered.txt"
+l = []
+parse(file, l)
+print(l)
 
 	
