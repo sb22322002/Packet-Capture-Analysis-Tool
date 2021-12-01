e04bea451f3c0ba4c3f477c4724c1d1427ec508c
arogoff <asr8607@rit.edu>
2021-12-01 14:14:46 -0500

    Update packet_parser.py
    - Created a create_list function. Takes in a string of data (in this case a line) and separates it into a list

--- packet_parser.py
+++ packet_parser.py
@@ -2,6 +2,26 @@ import re
 
 # !/usr/bin/env python3
 # taking file that needs to be parsed and outputting parsed list
+
+# Create_list - Takes in the string of data and separates it into a list
+def create_list(data):
+    number = data.split(' ')[0]
+    time = data.split(' ')[1]
+    source = data.split(' ')[2]
+    destination = data.split(' ')[3]
+    protocol = data.split(' ')[4]
+    length = data.split(' ')[5]
+
+    type_holder = data.split(',')[0].split(' ')[6:]
+    type = ""
+    for field in type_holder:
+            type += field + " "
+
+    seq = data.split(',')[1]
+    ttl = data.split(',')[2]
+
+    return [number, time, source, destination, protocol, length, type, seq, ttl]
+
 def parse():
     f1 = open("Node1_filtered.txt", "r")
     f2 = open("Node2_filtered.txt", "r")
@@ -16,11 +36,9 @@ def parse():
             if "No." in line:
                 line = i.readline()
                 new = re.sub("\s+", " ", line.strip())
-                ls.append(new)
+                ls.append(create_list(new))
         big_list.append(ls)
-    print(ls)
 
     return big_list
 
-parse()
 
